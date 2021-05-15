4c2acd30253e5578314458f112229e5516828a07
Developer58-JS <82396832+Developer58-JS@users.noreply.github.com>
2021-05-15 14:43:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+window.onload = function() {
+    var seconds = 00;
+    var tens = 00;
+    var appendTens = document.getElementById("tens");
+    var appendSeconds = document.getElementById("seconds");
+    var startBtn = document.getElementById("start");
+    var stopBtn = document.getElementById("stop");
+    var resetBtn = document.getElementById("reset");
+    var Interval;
+
+    startBtn.onclick = function() {
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 10);
+    }
+
+    stopBtn.onclick = function() {
+        clearInterval(Interval);
+    }
+
+    resetBtn.onclick = function() {
+        clearInterval(Interval);
+        tens = "00";
+        seconds = "00";
+        appendTens.innerHTML = tens;
+        appendSeconds.innerHTML = seconds;
+    }
+
+    function startTimer() {
+        tens ++
+        if (tens <= 9) {
+            appendTens.innerHTML = "0" + tens;
+        }
+        if (tens > 9){
+            appendTens.innerHTML = tens;
+            
+          } 
+          if (tens > 99) {
+            console.log("seconds");
+            seconds++;
+            appendSeconds.innerHTML = "0" + seconds;
+            tens = 0;
+            appendTens.innerHTML = "0" + 0;
+          }
+          if (seconds > 9){
+            appendSeconds.innerHTML = seconds;
+          }
+    }
+}
